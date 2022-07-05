8b24ec06c84cb84cb1db7e4e5382b290cff4ad67
ibra-tam <seyditamsir@gmail.com>
2022-07-05 11:15:25 +0200

    adding mongoDB

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ const express = require('express')
 const app = express();
 const port = 3000;
 
+var mongoose = require('mongoose');
+
+var mongoDB = '';
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+
+var db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
+
+
 app.get('/', (req, res) => {
   res.send('Hello World express!')
 });
